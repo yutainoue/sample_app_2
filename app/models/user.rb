074529801564552b325975c607e107731c336fcf
yutainoue <yutainoue009@gmail.com>
2021-02-17 02:50:07 +0000

    パスワードに最小文字数と空文字制限をかけた

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,15 @@ class User < ApplicationRecord
 
   before_save { email.downcase! }
 
-  validates :name,  presence:   true,
-                    length:     { maximum: 50 }
-  validates :email, presence:   true,
-                    length:     { maximum: 255 },
-                    format:     { with: VALID_EMAIL_REGEX },
-                    uniqueness: true
+  validates :name,     presence:   true,
+                       length:     { maximum: 50 }
+  validates :email,    presence:   true,
+                       length:     { maximum: 255 },
+                       format:     { with: VALID_EMAIL_REGEX },
+                       uniqueness: true
+
+  validates :password, presence: true,
+                       length: { minimum: 6 }
+
+  has_secure_password
 end
